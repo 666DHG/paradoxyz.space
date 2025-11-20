fbd172adf67472d389315eeb6fa9a61f0d47fa1a
666DHG <1265448316@qq.com>
2025-11-20 12:47:32 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://paradoxyz.space', 
+  integrations: [sitemap()],
+
+  markdown: {
+    shikiConfig: {
+      theme: 'css-variables',
+      langs: [],
+      wrap: true,
+    },
+  },
+
+  adapter: cloudflare(),
+  output: 'server'
+});
